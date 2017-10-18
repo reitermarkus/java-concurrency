e122a89606400fc31026685191ffe2c022a4ba87
Markus Reiter <me@reitermark.us>
2017-10-18 23:58:33 +0200

    Add second part of task 1 for exercise sheet 1.

---
+++ src/ex1/MaxThreads.java
@@ -0,0 +1,21 @@
+package ex1;
+
+public class MaxThreads {
+  public static void main(String[] args) {
+    int counter = 0;
+
+    while (true) {
+      try {
+        new Thread(() -> {
+          while (true) {
+            Thread.yield();
+          }
+        }).start();
+        counter++;
+      } catch (OutOfMemoryError e) {
+        System.out.println("Your system can handle " + counter + " threads.");
+        System.exit(0);
+      }
+    }
+  }
+}
