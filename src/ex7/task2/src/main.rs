5f6e40fb42d3effd67e259d3a7c86ea1687fce44
Markus Reiter <me@reitermark.us>
2018-01-19 22:06:56 +0100

    Complete `ex7.task2`.

--- src/ex7/task2/src/main.rs
+++ src/ex7/task2/src/main.rs
@@ -1,16 +1,54 @@
 use std::env;
-use std::path::Path;
-
-mod occurrence_count;
-
-use occurrence_count::occurrence_count;
+use std::fs::{File, read_dir};
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
 
 extern crate crossbeam;
 extern crate num_cpus;
 extern crate rayon;
 extern crate regex;
 
-use regex::RegexBuilder;
+use regex::{Regex, RegexBuilder};
+
+fn word_occurrences(re: &Regex, path: &Path) -> usize {
+  let file = match File::open(&path) {
+    Ok(file) => file,
+    Err(_) => panic!("Could not read '{}'!", path.to_str().unwrap())
+  };
+
+  let mut buf_reader = BufReader::new(file);
+
+  let mut content = String::new();
+
+  buf_reader.read_to_string(&mut content).unwrap();
+
+  re.find_iter(&content).count()
+}
+
+
+fn word_occurrences_in_dir(word: &Regex, path: &Path, n_threads : usize) -> usize {
+  rayon::initialize(rayon::Configuration::new().num_threads(n_threads)).unwrap();
+
+  fn join_rec(word: &Regex, paths: &[PathBuf]) -> usize {
+    paths.iter().fold(0, |sum, path| {
+      if path.is_file() {
+        sum + word_occurrences(&word, &path)
+      } else {
+        sum + match read_dir(path) {
+          Ok(entries) => {
+            let paths: Vec<_> = entries.map(|entry| entry.unwrap().path()).collect();
+            let (left, right) = paths.split_at(paths.len() / 2);
+            let (a, b) = rayon::join(|| join_rec(&word, left), || join_rec(&word, right));
+            a + b
+          },
+          Err(_) => panic!("Could not read directory '{}'!", path.as_path().to_str().unwrap())
+      }
+    }})
+  }
+
+  join_rec(&word, &[PathBuf::from(path)])
+}
 
 fn main() {
   let mut args = env::args().skip(1);
@@ -33,7 +71,7 @@ fn main() {
     None => panic!("No word given!")
   };
 
-  println!("Searching for '{}' in '{}' …", word, dir);
+  println!("Searching for occurrences of '{}' inside '{}' …", word, dir);
 
   let re = RegexBuilder::new(&word)
              .multi_line(true)
@@ -41,9 +79,7 @@ fn main() {
              .build()
              .unwrap();
 
-             println!("{:?}", re);
-
-  let word_count = occurrence_count(&re, &String::from("dir/document100.txt"));
+  let word_count = word_occurrences_in_dir(&re, &Path::new(&dir), 1);
 
   println!("{}", word_count);
 }
